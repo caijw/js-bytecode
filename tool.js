7d0c36cdfdfb4ea09a1babedcf16839483965a0b
kingweicai <kingweicai@tencent.com>
2022-01-14 01:12:02 +0800

    chore: update

--- tool.js
+++ tool.js
@@ -1,21 +1,58 @@
 const minimist = require('minimist')
 const fs = require('fs')
 const path = require('path')
+const assert = require('assert/strict')
 
 const argv = minimist(process.argv.slice(2))
-const logFile = argv.log
+const inFile = argv.in
 const outFile = argv.out
 const sourceFile = path.parse(argv.source)
 
-console.log(`sourceFile:`, sourceFile)
 
-const logContext = fs.readFileSync(logFile).toString()
+const inContent = fs.readFileSync(inFile).toString()
+const sourceContent = fs.readFileSync(argv.source).toString()
 
 const reg = /(evalmachine\.<anonymous>):(\d+):(\d+)/g
+const regLine = /(evalmachine\.<anonymous>):1\n/g
+let lastLine = -1
 
-var outLogContent = logContext.replace(reg, (match, p1, p2, p3, offset, string) => {
-    return `${sourceFile.name + sourceFile.ext}:${p2}:${p3}`
+function getRealPosition(line, column) {
+    assert.equal(line, 1)
+    let currentLineStart = 0
+    let realLine = 1
+    for (let i = 0; i < column; ++i) {
+        if (sourceContent[i] === '\n') {
+            realLine++;
+            currentLineStart = i
+        }
+    }
+    return {
+        line: realLine,
+        column: column - currentLineStart - 1
+    }
+}
+
+var outContent = inContent.replace(reg, (match, p1, p2, p3, offset, string) => {
+    const line = parseInt(p2, 10)
+    const column = parseInt(p3, 10)
+    const realPostion = getRealPosition(line, column)
+    if (lastLine === -1) {
+        lastLine = realPostion.line
+    }
+    return `${sourceFile.name + sourceFile.ext}:${realPostion.line}:${realPostion.column}`
 })
 
+outContent = outContent.replace(regLine, (match, p1, p2, offset, string) => {
+    return `${sourceFile.name + sourceFile.ext}:${lastLine}\n`
+})
 
-console.log('out:\n', outLogContent)
+console.log(inContent)
+
+if (outFile) {
+    fs.writeFileSync(outFile, outContent)
+} else {
+    console.log(outContent)
+}
+
+
+
